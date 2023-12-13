90dd5329a1e483039efeead7ca5854ab98bca5db
satya <satya.hyd11@gmail.com>
2023-12-13 17:05:05 +0530

    add syllables

--- install_dependecies.sh
+++ install_dependecies.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
-# Update package information
 apt update
-
-# Install Chromium and ChromeDriver
 apt install -y chromium-chromedriver
-
-# Install Selenium using pip
 pip install selenium
+pip install syllables
